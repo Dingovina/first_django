9ed5a917a0ead6feea85de2b0f108b1012a0b1a4
Dingovina <65973711+Dingovina@users.noreply.github.com>
2021-11-23 10:56:18 +0500

    Bootstrap вроде сделал, но не уверен, что он работает.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -16,9 +16,13 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-ovxxbh+47z&j(d#_+jw3it6)gn&(&8di!93u1p0h!0dy+#8x!_'
@@ -27,7 +31,6 @@ SECRET_KEY = 'django-insecure-ovxxbh+47z&j(d#_+jw3it6)gn&(&8di!93u1p0h!0dy+#8x!_
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,9 +78,7 @@ TEMPLATES = [
     },
 ]
 
-
 WSGI_APPLICATION = 'mysite.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
@@ -88,7 +89,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -108,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -121,7 +120,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
